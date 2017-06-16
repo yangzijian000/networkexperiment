1fb9f34e0ba6718fc738a26c6e456d21a837aa6a
Unknown <15979180165@163.com>
2017-06-16 22:21:09 +0800

    矢量路由算法仿真1.1
    矢量路由算法仿真1.1

--- road_main.py
+++ road_main.py
@@ -44,27 +44,39 @@ class DV_main_window(QtWidgets.QMainWindow,DV.Ui_MainWindow):
             self.routerdate.append('输入有误，请重新输入！')
     def InitDxy(self):
         for Nood in noodlist.values():
-            for nood in noodlist.values():
-                if nood.name in Nood.neighbors:
-                    Nood.Dxy[nood.name] = int(Nood.neighbors[nood.name])
+            for destination in noodlist.values():
+                if destination.name in Nood.neighbors:
+                    Nood.Dxy[destination.name] = int(Nood.neighbors[destination.name])
+                    Nood.nextnoods[destination.name] = destination.name
                 else:
-                    Nood.Dxy[nood.name] = INF
+                    Nood.Dxy[destination.name] = INF
+                    Nood.nextnoods[destination.name] = 'No_Path'
             Nood.Dxy[Nood.name] = 0
+            Nood.nextnoods[Nood.name] = Nood.name
         for Nood in noodlist.values():
             Nood.Bellman_Ford()
         self.programdate.append('#××××××××××××××××××××××××××#')
         self.programdate.append('#                      更新完毕！                    #')
         self.programdate.append('#××××××××××××××××××××××××××#')
         self.programdate.append('最终结果：')
-        for nood in noodlist.values():
-            self.programdate.append('节点{}的路由表为：'.format(nood.name))
-            str1 = '节点：'
-            str2 = '距离：'
-            for dxy in nood.Dxy.keys():
-                str1 = str1 + '{}  '.format(dxy)
-                str2 = str2 + '{}  '.format(nood.Dxy[dxy])
-            mainwindow.programdate.append(str1)
-            mainwindow.programdate.append(str2)
+        try:
+            for Nood in noodlist.values():
+                self.programdate.append('节点{}的路由表为：'.format(Nood.name))
+                str1 = '   |'
+                str2 = []
+                for nood in noodlist.values():
+                    str1 = str1 + '{} |'.format(nood.name)
+                    str = '{}  |'.format(nood.name)
+                    for destination in nood.Dxy.keys():
+                        str = str + '距离{} 下一跳{} |'.format(nood.Dxy[destination], nood.nextnoods[destination])
+                    str2.append(str)
+
+                mainwindow.programdate.append(str1)
+                mainwindow.programdate.append('---|-------------------')
+                for str in str2:
+                    mainwindow.programdate.append(str)
+        except Exception as e:
+            print(e)
 
 
 
@@ -73,47 +85,45 @@ class Nood(object):
         self.name = routername
         self.neighbors = neighbors
         self.Dxy = {}
-
+        self.nextnoods = {}
     def Bellman_Ford(self):
         Dxy_changed = False
         mainwindow.programdate.append('节点{}开始更新'.format(self.name))
         for nood in noodlist.values():
             if self.name == nood.name:
                 continue
-            Dxy = min(noodlist[neighbor].Dxy[nood.name]
-                                          + self.Dxy[neighbor]
-                      for neighbor in self.neighbors.keys())
-            if self.Dxy[nood.name] != Dxy:
-                mainwindow.programdate.append('到节点{}的距离由{}更新至{}'
-                                              .format(nood.name,self.Dxy[nood.name],Dxy))
-                self.Dxy[nood.name] = Dxy
+            Dxy_all = {}
+            for neighbor in self.neighbors.keys():
+                Dxy_all[neighbor] = noodlist[neighbor].Dxy[nood.name]+ self.Dxy[neighbor]
+            Dxy = sorted(Dxy_all.items(), key=lambda d: d[1])[0]
+            nextnood = Dxy[0]
+            if self.Dxy[nood.name] != Dxy[1]:
+                mainwindow.programdate.append('到节点{}的距离由{}更新至{},下一跳由{}更新至{}'
+                                              .format(nood.name,self.Dxy[nood.name],Dxy[1]
+                                                      ,self.nextnoods[nood.name],nextnood))
+                self.Dxy[nood.name] = Dxy[1]
+                self.nextnoods[nood.name] = nextnood
                 Dxy_changed = True
-        str1 = '节点：'
-        str2 = '距离：'
-        for dxy in self.Dxy.keys():
-            str1 = str1 + '{}  '.format(dxy)
-            str2 = str2 + '{}  '.format(self.Dxy[dxy])
+        str1 = '   |'
+        str2 = []
         mainwindow.programdate.append('节点{}的路由表更新后：'.format(self.name))
-        mainwindow.programdate.append(str1)
-        mainwindow.programdate.append(str2)
+        try:
+            for nood in noodlist.values():
+                str1 = str1 + '{} |'.format(nood.name)
+                str = '{}  |'.format(nood.name)
+                for destination in nood.Dxy.keys():
+                    str = str + '距离{} 下一跳{} |'.format(nood.Dxy[destination],nood.nextnoods[destination])
+                str2.append(str)
+
+            mainwindow.programdate.append(str1)
+            mainwindow.programdate.append('---|-------------------')
+            for str in str2:
+                mainwindow.programdate.append(str)
+        except Exception as e:
+            print(e)
         if Dxy_changed:
             for neighbor in self.neighbors.keys():
                 noodlist[neighbor].Bellman_Ford()
-
-    # def setneighbor(self, *neighbors):
-    #     for neighbor in neighbors:
-    #         self.neighbor.add(neighbor)
-    #
-    # def setnoodlist(self, *noodlist):
-    #     for nood in noodlist:
-    #         self.noodlist.add(nood)
-            # def setCxy(self):
-            #     for nood in self.noodlist
-            #         if nood is not in self.neighbor:
-            #             self.Cxy[nood] = 9999999
-
-            # def initDxy(self):
-            #     for nood in self.noodlist:
 
 if __name__ == '__main__':
     # text = '1:2,2:3,3:5,6:9'
